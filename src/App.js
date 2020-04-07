a7a1978218bcd2cd89866fd60f25356314730909
unknown <felipeAgomes>
2020-04-06 23:01:15 -0300

    criando formulário com Unform

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
+import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+import * as Yup from 'yup';
+
 import './App.css';
 
+import Input from './components/Form/Input';
+
+
+
 function App() {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }){
+    try {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('O nome é obrigatório'),
+      email: Yup.string()
+      .email('Digite um email válido')
+      .required('O email é obrigatório'),
+      address: Yup.object().shape({
+        city: Yup.string()
+        .min(3, 'No mínimo três caracteres')
+        .required('A cidade é obrigatória.')
+      })
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+    
+    console.log(data);
+    formRef.current.setErrors({});
+
+
+    reset();
+  } catch (err) {
+    if (err instanceof Yup.ValidationError){
+
+      const errorMessages = {};
+
+    err.inner.forEach(error => {
+      errorMessages[error.path] = error.message;
+    })
+    formRef.current.setErrors(errorMessages);
+  }
+  }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'Felipe Augusto',
+        email: 'felipeagomesdeoliveira@gmail.com',
+        address: {
+          city: 'São Paulo'
+        }
+      })
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!!</h1>
+
+    <Form ref={formRef} onSubmit={handleSubmit}>
+      <Input name="name" />
+      <Input type="email" name="email" />
+
+      <Scope path="address">
+      <Input name="street"/>
+      <Input name="neighborhood"/>
+      <Input name="city"/>
+      <Input name="state"/>
+      <Input name="number"/>
+      </Scope>
+
+      <button type="submit">Enviar</button>
+    </Form>
+
     </div>
   );
 }
